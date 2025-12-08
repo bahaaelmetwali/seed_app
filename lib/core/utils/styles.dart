038b65f6498cdb0fd88a75960ea0b2b6bcb8a5b3
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-12-08 14:34:01 +0200

    feat : styles added to project

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,47 +2,35 @@ import 'dart:ui';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class Styles {
-  static final TextStyle textStyle28 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
-    fontSize: 28.sp,
-    fontWeight: FontWeight.w700,
-  );
-
-  static final TextStyle textStyle20 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
-    fontSize: 20.sp,
-    fontWeight: FontWeight.w600,
-  );
-
-  static final TextStyle textStyle32 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
-    fontSize: 32.sp,
-    fontWeight: FontWeight.w600,
+  static final TextStyle textStyle24 = TextStyle(
+    fontFamily: 'Avenir',
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w800,
   );
 
   static final TextStyle textStyle14 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
+    fontFamily: 'Avenir',
     fontSize: 14.sp,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.w800,
   );
 
   static final TextStyle textStyle16 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
+    fontFamily: 'Avenir',
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
   );
   static final TextStyle textStyle18 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
+    fontFamily: 'Avenir',
     fontSize: 18.sp,
     fontWeight: FontWeight.w600,
   );
   static final TextStyle textStyle12 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
+    fontFamily: 'Avenir',
     fontSize: 12.sp,
     fontWeight: FontWeight.w600,
   );
   static final TextStyle textStyle10 = TextStyle(
-    fontFamily: 'IBMPlexSansArabic',
+    fontFamily: 'Avenir',
     fontSize: 10.sp,
     fontWeight: FontWeight.w400,
   );
--- lib/features/auth/domain/use_cases/login_use_case.dart
+++ lib/features/auth/domain/use_cases/login_use_case.dart
@@ -5,4 +5,5 @@ class LoginUseCase {
   LoginUseCase(this._authRepository) ;
   
 
+
 }
