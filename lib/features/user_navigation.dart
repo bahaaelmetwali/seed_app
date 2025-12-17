b326548df64fd2b8435288a0e856da1175ef7ad1
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-12-17 12:55:28 +0200

    Optimize screen loading with lazy initialization
    Wraps each navigation tab screen in a lazy loader to ensure only the selected tab's content is initialized and built. Improves performance by preventing unnecessary widget builds for inactive tabs and enhances app responsiveness.

--- lib/features/user_navigation.dart
+++ lib/features/user_navigation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:seed_app/constants/assets.dart';
 import 'package:seed_app/core/utils/constants.dart';
+import 'package:seed_app/core/widgets/lazy_page.dart';
 import 'package:seed_app/features/advertisements/presentation/screens/home_screen.dart';
 import 'package:seed_app/features/other_screens/add_advertisment.dart';
 import 'package:seed_app/features/other_screens/more_screen.dart';
@@ -22,12 +23,15 @@ class _UserHomeScreenState extends State<UserNavigation> {
     currentIndex = widget.currentIndex;
   }
 
-  final List<Widget> screens = [
-    HomeScreen(),
-    MyAdvertisment(),
-    AddAdvertisment(),
-    MoreScreen(),
-  ];
+  List<Widget> screens(int currentIndex) {
+    return [
+      LazyPage(isSelected: currentIndex == 0, child: HomeScreen()),
+      LazyPage(isSelected: currentIndex == 1, child: MyAdvertisment()),
+      LazyPage(isSelected: currentIndex == 2, child: AddAdvertisment()),
+      LazyPage(isSelected: currentIndex == 3, child: MoreScreen()),
+    ];
+  }
+
   late int currentIndex;
 
   @override
@@ -35,7 +39,10 @@ class _UserHomeScreenState extends State<UserNavigation> {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        body: IndexedStack(index: currentIndex, children: screens),
+        body: IndexedStack(
+          index: currentIndex,
+          children: screens(currentIndex),
+        ),
 
         bottomNavigationBar: Theme(
           data: Theme.of(context).copyWith(
